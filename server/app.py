263e3f651875c27850bea47cd4e97ce86fcbe918
AnitaNamjoon <anitachepkemoi1@gmail.com>
2023-09-26 20:25:05 +0300

    New commit

--- server/app.py
+++ server/app.py
@@ -1,19 +1,67 @@
-from flask import Flask, make_response
+from flask import Flask, request, render_template, redirect, flash, url_for
 from flask_migrate import Migrate
-
-from models import db, Author, Post
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.exc import IntegrityError, DataError
+from models import Author, Post
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key'  
 
+db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 db.init_app(app)
+
 
 @app.route('/')
 def index():
     return 'Validations lab'
+
+@app.route('/authors', methods=['GET', 'POST'])
+def authors():
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            phone_number = request.form['phone_number']
+            author = Author(name=name, phone_number=phone_number)
+            db.session.add(author)
+            db.session.commit()
+            flash('Author added successfully', 'success')
+        except IntegrityError:
+            db.session.rollback()
+            flash('Author with the same name already exists', 'danger')
+        except DataError:
+            db.session.rollback()
+            flash('Invalid phone number (must be 10 digits)', 'danger')
+
+    authors = Author.query.all()
+    return render_template('authors.html', authors=authors)
+
+@app.route('/posts', methods=['GET', 'POST'])
+def posts():
+    if request.method == 'POST':
+        try:
+            title = request.form['title']
+            content = request.form['content']
+            summary = request.form['summary']
+            category = request.form['category']
+            
+            post = Post(title=title, content=content, summary=summary, category=category)
+            db.session.add(post)
+            db.session.commit()
+            flash('Post added successfully', 'success')
+        except DataError:
+            db.session.rollback()
+            flash('Invalid input data', 'danger')
+        except ValueError as e:
+            db.session.rollback()
+            flash(str(e), 'danger')
+
+    posts = Post.query.all()
+    return render_template('posts.html', posts=posts)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,32 +1,49 @@
+from flask import Flask, request, render_template, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
-db = SQLAlchemy()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'your_secret_key' 
+
+db = SQLAlchemy(app)
 
 class Author(db.Model):
     __tablename__ = 'authors'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
-    name= db.Column(db.String, unique=True, nullable=False)
-    phone_number = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    name = db.Column(db.String, unique=True, nullable=False) 
+    phone_number = db.Column(db.String(10), nullable=False)  
 
-    def __repr__(self):
-        return f'Author(id={self.id}, name={self.name})'
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if len(phone_number) != 10:
+            raise ValueError("Phone number must be exactly ten digits")  
+        return phone_number
 
 class Post(db.Model):
     __tablename__ = 'posts'
-    # Add validations and constraints 
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    content = db.Column(db.String)
-    category = db.Column(db.String)
-    summary = db.Column(db.String)
-    created_at = db.Column(db.DateTime, server_default=db.func.now())
-    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    title = db.Column(db.String, nullable=False)  
+    content = db.Column(db.String, nullable=False)
 
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Post content must be at least 250 characters long")  
+        return content
+
+    summary = db.Column(db.String(250), nullable=False)  
+    category = db.Column(db.String, nullable=False, server_default='Fiction', onupdate='Fiction')  
 
     def __repr__(self):
-        return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
+        return f'Post(id={self.id}, title={self.title}, content={self.content}, summary={self.summary})'
+
+@app.route('/')
+def index():
+    return 'Validations lab'
+
+if __name__ == '__main__':
+    app.run(port=5555, debug=True)
